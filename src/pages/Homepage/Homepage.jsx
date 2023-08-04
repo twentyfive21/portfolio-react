8c4ed64ec20640b080207ebce7e5c047841a949f
Tina <tina.winnn@gmail.com>
2023-08-03 20:27:43 -0400

    found fonts for page

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Homepage.css'
+// import regular from "../../assets/regular.png"
+
+
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      <h1>Front end Developer</h1>
+      <p>Homepage</p>
+      {/* <img src={regular} /> */}
+    </div>
   )
 }
 
