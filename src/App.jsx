e58a70ef9293cdc024c8facd76fac94c841d2142
Tina <tina.winnn@gmail.com>
2023-08-03 20:35:22 -0400

    added header and footer starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Homepage from './pages/Homepage/Homepage'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <div>
+      <Header />
       <Homepage />
+      <Footer />
     </div>
   )
 }
