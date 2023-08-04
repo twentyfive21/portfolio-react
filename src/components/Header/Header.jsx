3e1d0be6ecefb50294a6458a70d1edb960d79edb
Tina <tina.winnn@gmail.com>
2023-08-03 20:49:33 -0400

    header links added temp

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,14 @@ import './Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header-container'>
+        <a href='#'>Tina Nguyen</a>
+        <div className='header-links'>
+        <a href='#'>Home</a>
+        <a href='#'>Projects</a>
+        <a href='#'>Connect</a>
+        </div>
+    </div>
   )
 }
 
