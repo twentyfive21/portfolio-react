4919123195f995ee27ff9e6e4538b39e2590d9eb
Tina <tina.winnn@gmail.com>
2023-08-04 20:00:04 -0400

    finalized which links for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,10 @@ import './Header.css'
 function Header() {
   return (
     <div className='header-container'>
-        <a href='#'>Tina Nguyen</a>
-        <div className='header-links'>
         <a href='#'>Home</a>
-        <a href='#'>Projects</a>
+        <a href='#'>Work</a>
+        <a href='#'>About</a>
         <a href='#'>Connect</a>
-        </div>
     </div>
   )
 }
