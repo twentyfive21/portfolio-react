ce4a1f636b96ed82efc5d3890b2a0fca0596d86d
Tina <tina.winnn@gmail.com>
2023-08-04 21:48:35 -0400

    imported flowers starting css

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './Homepage.css'
 // import regular from "../../assets/regular.png"
+import yellow from "../../assets/yellow.png"
+import red from "../../assets/red.png"
+import green from "../../assets/green.png"
 
 
 
 function Homepage() {
   return (
     <div className='intro-container'>
+      <div className='top-section'>
+      <img src={yellow} />
       <h1>Hi, I'm Tina <br/>A Frontend Developer.</h1>
-    
-      {/* <img src={regular} /> */}
+      <img src={green} />
+      </div>
+      <img src={red} />
     </div>
   )
 }
