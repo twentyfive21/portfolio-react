db943d623e98f08b042cac8376ce2fa341c9388b
Tina <tina.winnn@gmail.com>
2023-08-06 14:41:28 -0400

    added links to all 3 sites

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,9 +9,9 @@ import { BsFlower1 } from "react-icons/bs";
 function Footer() {
   return (
     <footer>
-      <FaGithub />
-      <BsLinkedin />
-      <BsFlower1 />
+      <a href='https://github.com/twentyfive21'><FaGithub /></a>
+      <a href='https://www.linkedin.com/in/tina-nguyen-393b7b237/'><BsLinkedin /></a>
+      <a href='https://bmc.link/twentyfive21'><BsFlower1 /></a>
     </footer>
   )
 }
