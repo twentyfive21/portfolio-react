f542ee20d75664963423a3f79efdd4c1c441743a
Tina <tina.winnn@gmail.com>
2023-08-06 14:36:39 -0400

    adding footer icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Footer.css'
+import { FaGithub } from "react-icons/fa6";
+import { BsLinkedin } from "react-icons/bs";
+import { BsFlower1 } from "react-icons/bs";
+
+// https://bmc.link/twentyfive21
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <FaGithub />
+      <BsLinkedin />
+      <BsFlower1 />
+    </footer>
   )
 }
 
