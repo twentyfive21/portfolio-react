914d0674dea1abcfb7ae697611c44454d3678c06
Tina <tina.winnn@gmail.com>
2023-08-06 17:04:11 -0400

    skills css section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Skills.css'
 import reactImg from '../../assets/react.png'
+import js from '../../assets/js.png'
+import css from '../../assets/css.png'
+import html from '../../assets/html.png'
+import figma from '../../assets/figma.png'
 
 
 function Skills() {
   return (
     <div className='skills'>
-       <img src={reactImg} alt='react'/>
+       <img src={reactImg} className='react-img' alt='react logo'/>
+       <img src={js} alt='javascript logo'/>
+       <img src={css} alt='css logo'/>
+       <img src={html} alt='html logo'/>
+       <img src={figma} alt='figma logo'/>
     </div>
   )
 }
