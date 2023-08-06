e8cdba84c9d17e4b4aa03b001ba81e97249eec0b
Tina <tina.winnn@gmail.com>
2023-08-06 15:52:08 -0400

    added skills component and css png

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Homepage from './pages/Homepage/Homepage'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Skills from './components/Skills/Skills'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Header />
       <Homepage />
+      <Skills />
       <Footer />
     </div>
   )
