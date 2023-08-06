cf5c0d0e89e364bd21899a1e34c02b16e797d2a6
Tina <tina.winnn@gmail.com>
2023-08-06 16:13:45 -0400

    imported test img

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Skills.css'
+import reactImg from '../../assets/react.png'
+
 
 function Skills() {
   return (
     <div className='skills'>
-        
+       <img src={reactImg} alt='react'/>
     </div>
   )
 }
