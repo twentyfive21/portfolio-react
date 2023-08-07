14fe895e574e82a25bd05226852c7484bf8f6532
Tina <tina.winnn@gmail.com>
2023-08-06 21:09:02 -0400

    added work css and filter tags

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -3,8 +3,14 @@ import './Work.css'
 
 function Work() {
   return (
-    <div>
-    
+    <div className='work-container'>
+      <h1>My Work</h1>
+     <div className='filter-container'>
+      <p>All</p>
+      <p>React</p>
+      <p>Javascript</p>
+      <p>HTML & CSS</p>
+    </div>
     </div>
   )
 }
