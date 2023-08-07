2d63cb8a5f9f1b5f37abbad03a11993053038de6
Tina <tina.winnn@gmail.com>
2023-08-06 21:07:52 -0400

    added alt tags for waria

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './Homepage.css'
 // import regular from "../../assets/regular.png"
 import yellow from "../../assets/yellow.png"
-import red from "../../assets/red.png"
 import green from "../../assets/green.png"
 
 
@@ -11,9 +10,9 @@ function Homepage() {
   return (
     <div className='intro-container'>
       <div className='top-section'>
-      <img src={yellow} className='flowers'/>
+      <img src={yellow} className='flowers' alt='flower'/>
       <h1>Hi, I'm Tina <br/>A Frontend Developer.</h1>
-      <img src={green} className='flowers' />
+      <img src={green} className='flowers' alt='flower'/>
       </div>
       <h3>Passionately creating captivating websites that foster meaningful connections.</h3>
     </div>
