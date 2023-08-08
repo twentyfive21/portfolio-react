481e806db260706d7ec78b31759fe1a4493bd38e
Tina <tina.winnn@gmail.com>
2023-08-08 19:41:37 -0400

    added map func to dsiaplay projects on filter

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import './Work.css'
 import data from '../../utilities/data'
 
 
 function Work() {
+const [selection, setSelection] = useState('');
+const userChoice = e => {setSelection(e.target.innerText)}
+
+const selectionData = data.filter(project => project.category == selection)
+
+useEffect(
+  () => {
+   setSelection('All')
+  },[]
+)
+
   return (
     <div className='work-container'>
       <h1>My Work</h1>
      <div className='filter-container'>
-      <p>All</p>
-      <p>React</p>
-      <p>Javascript</p>
-      <p>HTML & CSS</p>
+      <p onClick={userChoice}>All</p>
+      <p onClick={userChoice}>React</p>
+      <p onClick={userChoice}>Javascript</p>
+      <p onClick={userChoice}>HTML & CSS</p>
     </div>
     <div className='projects-container'>
       {
-        data.map(project => <img src={project.img} />)
+        selection == 'All' ?
+        data.map(project => <img src={project.img} key={project.key} />)
+        :
+        selectionData.map(project => <img src={project.img} key={project.key} />)
       }
     </div>
     </div>
