d1da1b9f3503b756377f63911a43c66765132cb6
Tina <tina.winnn@gmail.com>
2023-08-13 18:31:50 -0400

    added conatct text and email submission working

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,20 @@
 import './Contact.css'
+import React from 'react'
+
 function Contact() {
+
+// email function
+const handleClick = () => window.location.href = 'mailto:tinanguyen.connect@gmail.com';
+
   return (
-    <div>Contact</div>
-  )
+    <div className='contact-container'>
+      <h2>Contact Me</h2>
+      <p>I appreciate you taking the time to review my work. I'm eager to explore remote opportunities on your team. Please feel free to reach out if you have any remote roles or freelance work I can contribute to. I'd love to connect so we can stay in touch!</p>
+    <button onClick={handleClick}>
+      Send Email
+    </button>
+    </div>
+  );
 }
 
 export default Contact
