46f3fda1ce8749be62d3c689b74f59162786d398
Tina <tina.winnn@gmail.com>
2023-08-15 21:12:18 -0400

    added links and icons to contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,25 @@
 import './Contact.css'
 import React from 'react'
 import email from '../../assets/email.png'
+import { FaGithub } from "react-icons/fa6";
+import { BsLinkedin } from "react-icons/bs";
+import linkedin from '../../assets/linkedin.png'
+import github from '../../assets/github.png'
 
 
 function Contact() {
 
 // email function
-// const handleClick = () => window.location.href = 'mailto:tinanguyen.connect@gmail.com';
+const handleClick = () => window.location.href = 'mailto:tinanguyen.connect@gmail.com';
 
   return (
     <div className='contact-container'>
       <h2>Contact Me</h2>
       <p>I appreciate you taking the time to review my work. I'm eager to explore remote opportunities on your team. Please feel free to reach out if you have any remote roles or freelance work I can contribute to. I'd love to connect so we can stay in touch!</p>
       <div className='contact-links'>
-        {/* <button onClick={handleClick}><img src={email} /></button> */}
-        <p>Connect with me</p>
-        <p>View Github</p>
-        <img src={email} />
+      <a href='https://www.linkedin.com/in/tina-nguyen-393b7b237/'><img src={linkedin} /></a>
+      <button onClick={handleClick}><img src={email} alt='email icon'/></button>
+      <a href='https://github.com/twentyfive21'><img src={github} alt='github' /></a>
       </div>
     </div>
   );
