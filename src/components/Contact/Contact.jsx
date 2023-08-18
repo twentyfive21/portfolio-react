ad35a41a19d60656f3289bbc84779928d4fd093c
Tina <tina.winnn@gmail.com>
2023-08-17 22:40:08 -0400

    contact link added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
 const handleClick = () => window.location.href = 'mailto:tinanguyen.connect@gmail.com';
 
   return (
-    <div className='contact-container'>
+    <div id='contact-section' className='contact-container'>
       <h2>Contact Me</h2>
       <p>I appreciate you taking the time to review my work. I'm eager to explore remote opportunities on your team. Please feel free to reach out if you have any remote roles or freelance work I can contribute to. I'd love to connect so we can stay in touch!</p>
       <div className='contact-links'>
