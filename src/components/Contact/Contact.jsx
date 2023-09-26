8e4c04be711b1dcc028b3e2bbea32ddab4e4b9df
twentyfive21 <tina.winnn@gmail.com>
2023-09-26 15:06:01 -0400

    link updated for contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -17,7 +17,7 @@ const handleClick = () => window.location.href = 'mailto:tinanguyen.connect@gmai
       <h2>Contact Me</h2>
       <p>I appreciate you taking the time to review my work. I'm eager to explore remote opportunities on your team. Please feel free to reach out if you have any remote roles or freelance work I can contribute to. I'd love to connect so we can stay in touch!</p>
       <div className='contact-links'>
-      <a href='https://www.linkedin.com/in/tina-nguyen-393b7b237/'><img src={linkedin} /></a>
+      <a href='https://www.linkedin.com/in/tina-ngu'><img src={linkedin} /></a>
       <button onClick={handleClick}><img src={email} alt='email icon'/></button>
       <a href='https://github.com/twentyfive21'><img src={github} alt='github' /></a>
       </div>
