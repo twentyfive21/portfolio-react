e7f840f14a57fbbce841531802edafaf794c7c35
twentyfive21 <tina.winnn@gmail.com>
2024-02-21 14:34:06 -0500

    fixed vh issues on some mobile devices

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ function About() {
             <h2>About</h2>
           </section>
             <p>
-              With 1.5+ years of experience in front-end development, I can confidently develop responsive apps that deliver an unparalleled user experience. I strive for excellence in my work, diligently aiming for top-notch quality that consistently surpasses expectations. Known for being approachable and easy-going, I thrive in collaborative settings and excel at building strong relationships with colleagues.
+              With 2 years of experience in front-end development, I can confidently develop responsive apps that deliver an unparalleled user experience. I strive for excellence in my work, diligently aiming for top-notch quality that consistently surpasses expectations. Known for being approachable and easy-going, I thrive in collaborative settings and excel at building strong relationships with colleagues.
             </p>
         </div>
     </div>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ const handleClick = () => window.location.href = 'mailto:tinanguyen.connect@gmai
   return (
     <div id='contact-section' className='contact-container'>
       <h2>Contact Me</h2>
-      <p>I appreciate you taking the time to review my work. I am eager to explore remote opportunities on your team. Please feel free to reach out if you have any remote roles or freelance work I can contribute to.</p>
+      <p>I appreciate you taking the time to review my work. Please feel free to reach out if you have any remote roles or freelance work I can contribute to.</p>
         <div className='contact-links'>
           <button>
           <a href='https://www.linkedin.com/in/tina-ngu'><img src={linkedin} /></a>
